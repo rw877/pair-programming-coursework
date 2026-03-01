df2a7d270e497c06bbf6ff58c588ea1cf830efea
Raul Rus <raul.victor.rus@gmail.com>
2026-03-01 18:19:07 +0000

    Started getStatus method.

--- src/main/java/cityrescue/CityRescueImpl.java
+++ src/main/java/cityrescue/CityRescueImpl.java
@@ -26,6 +26,8 @@ public class CityRescueImpl implements CityRescue {
     private Unit[] units;
     private Incident[] incidents;
 
+    private int[] obstacles;
+
     // counters
     private int nextStation = 1;
     private int nextUnit = 1;
@@ -276,14 +278,30 @@ public class CityRescueImpl implements CityRescue {
     }
 
     @Override
+    // Tick updates movements, arrivals, on-scene, resolution.
     public void tick() {
         // TODO: implement
         throw new UnsupportedOperationException("Not implemented yet");
     }
 
     @Override
+    // TODO: Add variables for status, add status methods to Unit and Incident classes.
+    // Returns specifically formatted string as report of ticks, incidents and units.
     public String getStatus() {
-        // TODO: implement
-        throw new UnsupportedOperationException("Not implemented yet");
+        String status_report = "";
+        status_report += String.format("TICK=%d\nSTATIONS=%d UNITS=%d INCIDENTS=%d OBSTACLES=%d\n", ticks,
+                stations.length, units.length, incidents.length, obstacles.length);
+        status_report += "INCIDENTS\n";
+        // Loop adds formatted status of all the incidents.
+        for (int i = 0; i < incidents.length; i++) {
+            status_report += String.format("I#%d TYPE=%S SEV=%d LOC=(%d, %d) STATUS=%S UNIT=%d");
+        }
+        status_report += "UNITS\n";
+        // Loop adds formatted status of all the units.
+        for (int i = 0; i < units.length; i++) {
+            // TODO: Check condition for if working on scene to include number
+            status_report += String.format("U#%d TYPE=%S HOME=%d LOC=(%d, %d) STATUS=%S UNIT=%d");
+        }
+        return status_report;
     }
 }
