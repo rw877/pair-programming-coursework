f4d546fe4c22f65c48cf32ce7c7706370830e9ff
rw877 <rw877@exeter.ac.uk>
2026-03-01 18:14:44 +0000

    started incident class and methods

--- src/main/java/cityrescue/Incident.java
+++ src/main/java/cityrescue/Incident.java
@@ -1,9 +1,25 @@
 package cityrescue;
 
+import cityrescue.enums.IncidentStatus;
+import cityrescue.enums.IncidentType;
+
+
 public class Incident {
-    private int severity;
+    private IncidentType type;
     private int incidentId;
-    private int x_coordinate;
-    private int y_coordinate;
+    private int x;
+    private int y;
+    private int severity;
+    public IncidentStatus status; // examples of encapsulation
+
     
+    public Incident(int incidentId, IncidentType type, int severity, int x, int y) {
+        this.incidentId = incidentId;
+        this.type = type;
+        this.severity = severity;
+        this.x = x;
+        this.y = y;
+        this.status = IncidentStatus.REPORTED; 
+
+    }
 }
